02fb854769f15f517b73fb02a54f7b2f54ad128b
hasnainahamed24 <hasnainahamed24@gmail.com>
2026-09-25 23:46:45 +0600

    final project part-5

--- index.js
+++ index.js
@@ -113,8 +113,71 @@ async function run() {
 
         // company related apis
 
+        // app.get('/api/companies', async (req, res) => {
+        //     const cursor = companyCollection.find().skip(8);
+        //     const result = await cursor.toArray();
+        //     res.send(result);
+        // })
+
+
+        // inefficient way to join/aggregate collection
         app.get('/api/companies', async (req, res) => {
-            const cursor = companyCollection.find().skip(8);
+            const cursor = companyCollection.find();
+            const companies = await cursor.toArray();
+
+            for(const company of companies){
+                const filter = {
+                    companyId: company._id.toString()
+                }
+                const jobCount = await jobCollection.countDocuments(filter)
+                company.jobCount= jobCount
+
+            }
+
+            res.send(companies);
+        })
+
+
+        // inefficient way to join/aggregate collection
+        app.get('/api/companies2', async (req, res) => {
+            const pipeline = [
+                {
+                    $skip: 5
+                },
+                {
+                    $limit: 2
+                }
+            ];
+
+            const cursor = companyCollection.aggregate(pipeline);
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+
+        app.get('/api/stats', async(req, res) => {
+            const pipeline = [
+                {
+                    $group: {
+                        _id: '$type',
+                        count: {
+                            $sum: 1
+                        }
+                    }
+                },
+                {
+                    $project: {
+                        type: '$_id',
+                        _id: 0,
+                        count: 1
+                    }
+                },
+                {
+                    $sort: { count: 1 }
+                }
+            ]
+
+            const cursor = jobCollection.aggregate(pipeline);
             const result = await cursor.toArray();
             res.send(result);
         })
